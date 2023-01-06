bf24a9bfc4cecf47c942aa4bded2d0fe8ea79f57
amit <amitgangradeit001@gmail.com>
2023-01-06 16:43:46 +0530

    Initial commit
    Sample project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,30 @@
+import java.util.TreeSet
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+
+    val a = isPalindrome("amittim")
+
+    println("To check palindrome string $a")
+}
+fun isPalindrome(str:String):Boolean{
+    var rev =""
+    var ans = false
+
+
+    for(i in str.length-1 downTo 0){
+
+        rev += str[i]
+
+    }
+    if (str == rev){
+        ans = true
+    }
+
+    return ans
+}
+
